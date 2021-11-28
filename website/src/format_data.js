4df044a67eeccf6556e753be5da547212f9f65f0
Noah E <nepstein@college.harvard.edu>
2021-11-28 15:54:52 -0500

    add in files from agi's branch

---
+++ website/src/format_data.js
@@ -0,0 +1,27 @@
+import {team, attShotDist, madeShotDist} from './bubble_chart_nba';
+
+export const compareBy = (category) => {
+    switch (category) {
+        case "team":
+            return (a,b) => {
+                if (a[team] < b[team]) return -1;
+                if (a[team] > b[team]) return 1;
+                return 0;
+            }
+        case "attShotDist":
+            return (a, b) => {
+                if (a[attShotDist] < b[attShotDist]) return -1;
+                if (a[attShotDist] > b[attShotDist]) return 1;
+                return 0;
+            }
+
+        case "madeShotDist":
+            return (a, b) => {
+                if (a[madeShotDist] < b[madeShotDist]) return -1;
+                if (a[madeShotDist] > b[madeShotDist]) return 1;
+                return 0;
+            }
+        default:
+            break;
+    }
+}
