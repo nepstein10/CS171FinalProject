27085d5c965a982e3816f4045ba0d05b1e12ac80
Arda514 <acataltepe@college.harvard.edu>
2021-11-28 18:56:33 -0500

    Started implementing fullpage scrolling

--- website/js/scroller.js
+++ website/js/scroller.js
@@ -1,6 +1,5 @@
-import Splide from '@splidejs/splide';
-new Splide( '.splide' ).mount();
+new fullpage('#fullpage', {
+    sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE'
+});
 
-new Splide( '#slider1' ).mount();
-new Splide( '#slider2' ).mount();
-new Splide( '#slider3' ).mount();
